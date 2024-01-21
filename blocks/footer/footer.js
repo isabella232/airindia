b27b3690cdd96c209f2b8ae23e44a82c8cb45d75
abhishekshar-adobe <139372022+abhishekshar-adobe@users.noreply.github.com>
2024-01-21 11:50:44 +0530

    Footer changes #3 (#17)
    * Footer changes #3

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,5 +1,54 @@
-import { getMetadata } from '../../scripts/aem.js';
+import { getMetadata, createOptimizedPicture } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
+
+function createAccordion() {
+  const acc = document.querySelectorAll('.footer-top-item h6');
+  let i;
+
+  for (i = 0; i < acc.length - 1; i += 1) {
+    acc[i].addEventListener('click', ({ currentTarget }) => {
+      currentTarget.classList.toggle('active');
+      const panel = currentTarget.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = `${panel.scrollHeight}px`;
+      }
+    });
+  }
+}
+
+function decorateFooter(block, selectorClass) {
+  const footerTop = block.querySelector(`.${selectorClass}`);
+  const tempDiv = footerTop.querySelector('.default-content-wrapper');
+  const children = [...footerTop.querySelector('.default-content-wrapper').children];
+  let index = 0;
+  tempDiv.innerHTML = '';
+
+  while (index < children.length) {
+    const topItem = document.createElement('div');
+    topItem.classList.add(`${selectorClass}-item`);
+    // Skip h6 if title not available
+    if (children[index].innerText !== '_blank') { topItem.appendChild(children[index]); }
+    index += 1;
+
+    while (index < children.length) {
+      if (children[index].tagName === 'H6') {
+        if (!children[index + 1] || (children[index - 1].tagName === 'H6' && children[index + 1].tagName !== 'UL')) {
+          topItem.appendChild(children[index]);
+        } else {
+          break;
+        }
+      } else {
+        topItem.appendChild(children[index]);
+      }
+      index += 1;
+    }
+
+    tempDiv.appendChild(topItem);
+  }
+  tempDiv.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+}
 
 /**
  * loads and decorates the footer
@@ -18,4 +67,7 @@ export default async function decorate(block) {
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
   block.append(footer);
+  decorateFooter(block, 'footer-top');
+  decorateFooter(block, 'footer-middle');
+  createAccordion();
 }
