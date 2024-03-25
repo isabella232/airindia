ed14ab307937eb71e5a9eb8149d5a1ed0af0ded8
kalimuthu-a <155428518+kalimuthu-a@users.noreply.github.com>
2024-03-25 13:40:59 +0530

    fixed the api call (#55)
    * fixed the api call
    
    * removed the commented code
    
    * booking css fix added

--- scripts/utils/headerUtils.js
+++ scripts/utils/headerUtils.js
@@ -1,4 +1,52 @@
-// import memebershipJson from '../mock/getMembership.js';
+/* eslint-disable */
+function _utf8_decode(e) {
+  let a = '';
+  let t = 0;
+  let n = 0;
+  let i = 0;
+  for (c3 = 0; t < e.length;) {
+    (n = e.charCodeAt(t)),
+    n < 128
+      ? ((a += String.fromCharCode(n)), t++)
+      : n > 191 && n < 224
+        ? ((i = e.charCodeAt(t + 1)),
+        (a += String.fromCharCode(((31 & n) << 6) | (63 & i))),
+        (t += 2))
+        : ((i = e.charCodeAt(t + 1)),
+        (c3 = e.charCodeAt(t + 2)),
+        (a += String.fromCharCode(
+          ((15 & n) << 12) | ((63 & i) << 6) | (63 & c3),
+        )),
+        (t += 3));
+  }
+  return a;
+}
+function decode(e) {
+  let a;
+  let t;
+  let n;
+  let i;
+  let r;
+  let o;
+  let c;
+  const d = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+  let l = '';
+  let m = 0;
+  for (e = e.replace(/[^A-Za-z0-9\+\/\=]/g, ''); m < e.length;) {
+    (i = d.indexOf(e.charAt(m++))),
+    (r = d.indexOf(e.charAt(m++))),
+    (o = d.indexOf(e.charAt(m++))),
+    (c = d.indexOf(e.charAt(m++))),
+    (a = (i << 2) | (r >> 4)),
+    (t = ((15 & r) << 4) | (o >> 2)),
+    (n = ((3 & o) << 6) | c),
+    (l += String.fromCharCode(a)),
+    o != 64 && (l += String.fromCharCode(t)),
+    c != 64 && (l += String.fromCharCode(n));
+  }
+  return (l = _utf8_decode(l)), l;
+}
+/* eslint-enable */
 
 export function getLang() {
   return document.documentElement.lang;
@@ -12,33 +60,55 @@ export function getPlaceholderDataFor(key) {
 
 // Temporary placeholder function to mimic the signin behaviour. Will be refactored
 export function isLoggedIn() {
-  // return !!window.sessionStorage.getItem('accessToken');
-  // Temporarily to mimic the login experience
-  return true;
+  return !!window.sessionStorage.getItem('accessToken');
 }
 
 // Temporary placeholder function to mimic the signin behaviour. Will be refactored
 export async function getMembership() {
-  const memebershipUrl = getPlaceholderDataFor('getmemberships');
-  const memebershipResponse = await fetch(memebershipUrl);
-  const membershipData = await memebershipResponse.json();
-  return membershipData;
+  try {
+    const membershipUrl = getPlaceholderDataFor('getmemberships');
+    const token = decode(window.sessionStorage.getItem('accessToken'));
+    const subscriptionKey = getPlaceholderDataFor('ocpSubscriptionKey');
+
+    const response = await fetch(membershipUrl, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Ocp-Apim-Subscription-Key': subscriptionKey,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({}),
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch membership data');
+    }
+
+    return await response.json();
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 }
 
 function getInititals(firstName, lastName) {
-  return String(firstName?.[0]?.toUpperCase()) + String(lastName?.[0]?.toUpperCase());
+  return (
+    String(firstName?.[0]?.toUpperCase()) + String(lastName?.[0]?.toUpperCase())
+  );
 }
 
 // Temporary placeholder function to mimic the signin behaviour. Will be refactored
 export async function getUserInfo() {
   const membershipData = await getMembership();
-  const { responsePayload: { data: [userData] } } = membershipData;
+  const {
+    responsePayload: {
+      data: [userData],
+    },
+  } = membershipData;
   const {
     individual: {
       identity: {
-        name: {
-          romanized,
-        },
+        name: { romanized },
       },
     },
     membershipId,
@@ -46,9 +116,7 @@ export async function getUserInfo() {
       emails: [email],
     },
     loyaltyAward: [loyalty],
-    mainTier: {
-      label: club,
-    },
+    mainTier: { label: club },
   } = userData;
   return {
     initials: getInititals(romanized?.firstName, romanized?.lastName),
