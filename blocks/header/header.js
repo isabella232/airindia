427651c47e47dd0fc88d0aba375ba8cca2a021cc
Satya Deep Maheshwari <satyam@adobe.com>
2024-05-22 09:50:39 +0530

    Adding Assets and Gen AI plugin. Remove drafts nav

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -478,7 +478,7 @@ export default async function decorate(block) {
   // load nav as fragment
   const navMeta = getMetadata('nav');
   // TODO: Remove draft path before merge
-  const navPath = navMeta ? new URL(navMeta).pathname : '/drafts/nav';
+  const navPath = navMeta ? new URL(navMeta).pathname : '/nav';
   const fragment = await loadFragment(navPath);
 
   // decorate nav DOM
