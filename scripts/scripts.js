115a480fd732b693964cff613d70b825fbb72ce3
Satya Deep Maheshwari <m.satyadeep@gmail.com>
2024-06-21 14:19:08 +0530

    Remove hero auto blocking and add h1 support in hero block (#122)
    * Remove hero auto blocking and add h1 support in hero block
    ---------
    
    Co-authored-by: Satya Deep Maheshwari <satyam@adobe.com>

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,6 @@
 // This is a test comment
 import {
   sampleRUM,
-  buildBlock,
   loadHeader,
   loadFooter,
   decorateButtons,
@@ -36,21 +35,6 @@ export function getEnvType(hostname = window.location.hostname) {
 }
 
 /**
- * Builds hero block and prepends to main in a new section.
- * @param {Element} main The container element
- */
-function buildHeroBlock(main) {
-  const h1 = main.querySelector('h1');
-  const picture = main.querySelector('picture');
-  // eslint-disable-next-line no-bitwise
-  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
-    const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
-    main.prepend(section);
-  }
-}
-
-/**
  * load fonts.css and set a session storage flag
  */
 async function loadFonts() {
@@ -59,19 +43,6 @@ async function loadFonts() {
     if (!window.location.hostname.includes('localhost')) sessionStorage.setItem('fonts-loaded', 'true');
   } catch (e) {
     // do nothing
-  }
-}
-
-/**
- * Builds all synthetic blocks in a container element.
- * @param {Element} main The container element
- */
-function buildAutoBlocks(main) {
-  try {
-    buildHeroBlock(main);
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error('Auto Blocking failed', error);
   }
 }
 
@@ -124,7 +95,6 @@ export function decorateMain(main) {
   // hopefully forward compatible button decoration
   decorateButtons(main);
   decorateIcons(main);
-  buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
   decorateExternalLinks(main);
